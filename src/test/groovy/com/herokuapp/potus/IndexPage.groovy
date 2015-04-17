c2d4d6d7d9284b9de76c30828daa68a65ff5bccb
Steve Good <steve.good@bnsf.com>
2015-04-17 14:09:31 -0500

    Attempting to fix the test for travis

--- src/test/groovy/com/herokuapp/potus/IndexPage.groovy
+++ src/test/groovy/com/herokuapp/potus/IndexPage.groovy
@@ -3,7 +3,7 @@ package com.herokuapp.potus
 import geb.Page
 
 class IndexPage extends Page {
-  static url = '/'
+  static url = System.getProperty('UI_URL')
 
   static at = {
     title == 'Presidents of the United States'
